93f35ae0b1a9bab32df1e770dd5423c064a55097
sowjanya <sowji.muppala@gmail.com>
2025-11-03 10:22:30 +0530

    practice

--- 01.number.sh
+++ 01.number.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# i=2
-# n=10
-# ans = $(( n / i ))
-# echo $ans
-
-num1=10
-num2=2
-
-ans=$((num1 / num2))
-
+i=2
+n=20
+ans = $((n / i))
 echo $ans
+
+# num1=10
+# num2=2
+
+# ans=$((num1 / num2))
+
+# echo $ans
