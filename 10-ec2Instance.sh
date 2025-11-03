197aa209995cc9d5fa378b4c326f1d9df6acfab1
sowjanya <sowji.muppala@gmail.com>
2025-11-03 18:20:43 +0530

    practice

---
+++ 10-ec2Instance.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0696dc92e81135eee"
+ZONE_ID="Z09288351F1RG63CMSZ8A"
+DOMAIN_NAME="msdevsecops.fun"
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --key-name <your-key-pair-name> \
+    --security-group-ids $SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test}]'
+    --query 'Instances[0].InstanceId' \
+    --output text
