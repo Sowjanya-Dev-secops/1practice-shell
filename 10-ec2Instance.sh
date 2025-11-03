a6f3762587af801ca306ce18d78a1590da6a7dda
sowjanya <sowji.muppala@gmail.com>
2025-11-03 21:31:04 +0530

    practice

--- 10-ec2Instance.sh
+++ 10-ec2Instance.sh
@@ -9,9 +9,9 @@ do
     instance_id=$(aws ec2 run-instances --image-id "ami-09c813fb71547fc4f"  --instance-type "t3.micro" --security-group-ids "sg-0696dc92e81135eee" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ];then
-        ip=$(  aws ec2 describe-instances  --instance-ids i-0b99be5254401d43d --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text)
+        ip=$(  aws ec2 describe-instances  --instance-ids $instance_id --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text)
     else
-        ip=$(  aws ec2 describe-instances  --instance-ids i-0b99be5254401d43d --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text)
+        ip=$(  aws ec2 describe-instances  --instance-ids $instance_id --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text)
     fi
     echo "$instance : $ip"
 done
