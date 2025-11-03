62a90806019a57cc253e466c63d6f03040d87168
sowjanya <sowji.muppala@gmail.com>
2025-11-03 18:13:36 +0530

    practice

--- 09-colors.sh
+++ 09-colors.sh
@@ -12,7 +12,6 @@ LOG_FOLDER="/var/log/1practie-shell"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 mkdir -p $LOG_FOLDER
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-echo "$LOG_FILE"
 
 VALIDATE(){
     if [ $1 -ne 0 ];then
