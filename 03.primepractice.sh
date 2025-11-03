f353c26c34c655167c8b139742298e27d3ccb8d1
sowjanya <sowji.muppala@gmail.com>
2025-11-03 10:47:13 +0530

    practice

--- 03.primepractice.sh
+++ 03.primepractice.sh
@@ -3,8 +3,8 @@ echo -e "Enter the number: \c"
 read n
 for (( i=2; i<=n/i; i++ ))
 do
-    ans=$( n%i )
-    if [ $ans -eg 0 ]; then
+    ans=$(( n%i ))
+    if [ $ans -eq 0 ]; then
         echo "$n is not prime number"
         exit 0
     fi
