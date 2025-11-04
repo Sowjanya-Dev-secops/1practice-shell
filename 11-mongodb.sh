64329a884f78b76b440f6a4b7c2639745cf81de3
sowjanya <sowji.muppala@gmail.com>
2025-11-04 14:11:48 +0530

    practice

---
+++ 11-mongodb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+current_path=$( PWD )
+USERID=$( id -u )
+if [ $USERID -ne 0 ];then
+    echo " please proceed with root user previliage"
+fi
+
+LOG_FOLDER="/var/log/1practie-shell"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+mkdir -p $LOG_FOLDER
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo -e "$R error $N: $2 is not installed"
+    else
+        echo -e "$G success:$N $2 installation is succecssful"
+    fi
+}
+
+cp $current_path/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copy mongo repo file"
+
+dnf install mongodb-org -y 
+VALIDATE $? "mongodb-org"
+
+systemctl enable mongod
+VALIDATE $? "enable mongodb"
+
+systemctl start mongod 
+VALIDATE $? " start mongodb-org"
+
+sed /s/127.0.0.1/0.0.0.0/ /etc/mongod.conf
+
+systemctl restart mongod
+VALIDATE $? "reastart mongodb"
